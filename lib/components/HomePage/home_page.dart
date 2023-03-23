5e036334a781d3394cf1bae019878239617cbb60
Umut Kaan Kartaloğlu <umutkaankartaloglu9@gmail.com>
2023-03-23 13:34:22 +0300

    güncelleme yapıldı

--- lib/components/HomePage/home_page.dart
+++ lib/components/HomePage/home_page.dart
@@ -1,10 +1,12 @@
 import 'dart:ffi';
 
-import 'package:calorie_tracker/components/SearchPage/search_page.dart';
+import 'package:calorie_tracker/view/SplashScreen/SearchPage/search_page.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
 import 'package:sleek_circular_slider/sleek_circular_slider.dart';
 
@@ -13,19 +15,39 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double value =1500;
+    double value = 2000;
     return Scaffold(
       backgroundColor: Colors.white,
       body: Container(
         width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height * 0.7,
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Color.fromARGB(255, 226, 100, 50)),
+        height: MediaQuery.of(context).size.height * 0.58,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(60),
+                bottomRight: Radius.circular(60)),
+            color: Color.fromARGB(255, 226, 100, 50)),
         child: SafeArea(
           child: Center(
             child: Column(
               children: [
-                const Padding(
-                  padding: EdgeInsets.only(top: 40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SearchPage()));
+                        },
+                        icon: Icon(
+                          Icons.search,
+                          color: Colors.white,
+                          size: 30,
+                        )),
+                  ],
+                ),
+                Center(
                   child: Text(
                     "Today",
                     style: TextStyle(
@@ -37,42 +59,52 @@ class HomePage extends StatelessWidget {
                 SizedBox(
                   height: 50,
                 ),
-               
                 Column(
                   children: [
-                    Stack(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(top: 120,left: 80),
-                          child: Text("${value.toInt().toString()} %",style: TextStyle(fontSize: 45,color: Colors.white),),
-                        ),
-                        SleekCircularSlider(
-                          initialValue: value,
-                          max: 2000,
-                          appearance: CircularSliderAppearance(
-                              size: MediaQuery.of(context).size.width * 0.7,
-                              //  startAngle: 100,
-                              // angleRange: 100,
-                              animationEnabled: false,
-                             spinnerMode: true,
-                              
-                             customColors: CustomSliderColors(trackColor: Colors.grey,progressBarColor: Colors.yellow ),
-                             customWidths: CustomSliderWidths(handlerSize: 0,progressBarWidth: 10,trackWidth: 15)
-                              ),
-                          onChange: (double value) {
-                          },
-                        ),
-                      ],
+                    CircularPercentIndicator(
+                      radius: 120.0,
+                      lineWidth: 13.0,
+                      animation: true,
+                      percent: 0.7,
+                      center: Text(
+                        "${value.toInt().toString()} Kalori",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20.0),
+                      ),
+                      circularStrokeCap: CircularStrokeCap.round,
+                      progressColor: Colors.yellow,
                     ),
                   ],
-                )
+                ),
               ],
             ),
           ),
         ),
       ),
-      );
-    
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.white,
+        color: Color.fromARGB(255, 226, 100, 50),
+        animationDuration: Duration(microseconds: 200),
+        onTap: (index) {
+          print(index);
+        },
+        items: [
+          IconButton(
+            icon: Icon(
+              Icons.home,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.person,
+              color: Colors.white,
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
   }
 }
-
