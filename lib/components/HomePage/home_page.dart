8152293a582ca05474e1a0d58e3e55cce3169e9e
Fatmanur Polat <82556567+fatmanurpolat@users.noreply.github.com>
2023-03-23 16:26:51 +0300

    page route

--- lib/components/HomePage/home_page.dart
+++ lib/components/HomePage/home_page.dart
@@ -1,17 +1,19 @@
 import 'dart:ffi';
-
-import 'package:calorie_tracker/view/SplashScreen/SearchPage/search_page.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:calorie_tracker/components/HomePage/breakfast_page.dart';
+import 'package:calorie_tracker/components/HomePage/dinner_card.dart';
+import 'package:calorie_tracker/components/HomePage/lunch_card.dart';
+import 'package:calorie_tracker/components/ProfilePage/profile_page.dart';
+import 'package:calorie_tracker/view/SearchPage/search_page.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:get/get.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
 import 'package:sleek_circular_slider/sleek_circular_slider.dart';
-
-import '../../view/SplashScreen/SearchPage/search_page.dart';
-import 'dinner_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -27,68 +29,81 @@ class _HomePageState extends State<HomePage> {
     double value = 2000;
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height * 0.58,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(60),
-                bottomRight: Radius.circular(60)),
-            color: Color.fromARGB(255, 226, 100, 50)),
-        child: SafeArea(
-          child: Center(
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+      body: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.58,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(60),
+                    bottomRight: Radius.circular(60)),
+                color: Color.fromARGB(255, 226, 100, 50)),
+            child: SafeArea(
+              child: Center(
+                child: Column(
                   children: [
-                    IconButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => SearchPage()));
-                        },
-                        icon: Icon(
-                          Icons.search,
-                          color: Colors.white,
-                          size: 30,
-                        )),
-                  ],
-                ),
-                Center(
-                  child: Text(
-                    "Today",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 45,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                Column(
-                  children: [
-                    CircularPercentIndicator(
-                      radius: 120.0,
-                      lineWidth: 13.0,
-                      animation: true,
-                      percent: 0.7,
-                      center: Text(
-                        "${value.toInt().toString()} Kalori",
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        IconButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SearchPage()));
+                            },
+                            icon: Icon(
+                              Icons.search,
+                              color: Colors.white,
+                              size: 30,
+                            )),
+                      ],
+                    ),
+                    Center(
+                      child: Text(
+                        "Today",
                         style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20.0),
+                            color: Colors.white,
+                            fontSize: 45,
+                            fontWeight: FontWeight.bold),
                       ),
-                      circularStrokeCap: CircularStrokeCap.round,
-                      progressColor: Colors.yellow,
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Column(
+                      children: [
+                        CircularPercentIndicator(
+                          radius: 120.0,
+                          lineWidth: 13.0,
+                          animation: true,
+                          percent: 0.7,
+                          center: Text(
+                            "${value.toInt().toString()} Kalori",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20.0),
+                          ),
+                          circularStrokeCap: CircularStrokeCap.round,
+                          progressColor: Colors.yellow,
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
+              ),
             ),
           ),
-        ),
+          SizedBox(
+            height: 50,
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [BreakFastCard(), LunchCard(), DinnerCard()],
+            ),
+          )
+        ],
       ),
       bottomNavigationBar: CurvedNavigationBar(
         backgroundColor: Colors.white,
@@ -101,8 +116,11 @@ class _HomePageState extends State<HomePage> {
             if (_index == 0) {
               return;
             } else
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => ProfilPage()));
+              Navigator.push(
+                  context,
+                  PageTransition(
+                      type: PageTransitionType.leftToRight,
+                      child: ProfilPage()));
           });
         },
         items: [
